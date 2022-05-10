91cd8bd40e541ddcb9563ce51d1a8b68724d9731
花园路 <1262525903@qq.com>
2022-05-10 12:13:32 +0800

    注册
    等前端有注册页面以后把函数接口引用过去就行
    数据可能还得有一些修改 到时候在测试

--- miniprogram/pages/gameMarketProject/login/login.js
+++ miniprogram/pages/gameMarketProject/login/login.js
@@ -1,6 +1,7 @@
 // pages/gameMarketProject/login/login.js
 /*Author 花园路 */
-var app = getApp();
+const app = getApp();
+const db = wx.cloud.database();
 Page({
     
   /**
@@ -12,34 +13,28 @@ Page({
         Account_new: '',
         Password_new:'',
         Password_new_again:'',
-        Is_loading:false,
-        Is_weixin_login_failed:false,
-        Is_registering:false,
-        Is_name_illegal:false,
-        Is_registered:false,
-        password_illegal:false,
-        Is_different:false
+        IDNext:0,
     },
     
   Login :function(e) {
-    console.log("Login")
-    console.log(e.detail.value)
+    //console.log("Login")
+    //console.log(e.detail.value)
     //检查Password是否对应Account
     this.setData({
       Account:e.detail.value.username,
       Password:e.detail.value.password
     })
-    wx.cloud.database().collection("Users")
+    db.collection("Users")
     .where({
       Name : this.data.Account,
       Password : this.data.Password
     })
     .get({
       success :res=>{
-        console.log(res.data)
-      //getApp().globalData.user = JSON.parse(res.data)  需要在app.js中设置globalData user https://www.csdn.net/tags/MtjakgxsMzYyMDYtYmxvZwO0O0OO0O0O.html
+        //console.log(res.data)
         if(res.data.length>0){
-          console.log("登录成功")
+          app.globalData.User = res.data
+          //console.log(app.globalData.User)
           wx.reLaunch({
             url:"../index/index",
           })
@@ -60,9 +55,74 @@ Page({
     })
   },
 
-  Register :function() {
-    console.log("Register")
-    
+  Register :function(e) { //添加到前端Register页面的相关按钮接口
+//      /*
+//     判断密码长度是够合法，两次密码是否一致，设置相关布尔值
+//     判断Account——new是否合法，比较数据库账户是否已存在
+//     有任何错误则结束
+//     *
+//     都成功则依据数据库中的ID序号生成下一序号,
+//     设置app.globalData.user为新建的用户Json格式数据,
+//     并在user添加该用户,最后跳转至index  
+//     *
+//     */
+//     console.log("Register")
+//     this.setData({
+//       Account_new:e.detail.value.Account_new,
+//       Password_new:e.detail.value.Password_new,
+//       Password_new_again:e.detail.value.Password_new_again
+//     })
+//     if(this.data.Password_new!=this.data.Password_new_again) {
+//       wx.showToast({
+//         title: '两次输入密码不同请重新输入',
+//         icon:'error',
+//         duration: 1500,
+//       })
+//     }
+//     else {
+//       db.collection("Users")
+//       .where({
+//         Name:this.data.Account_new
+//       })
+//       .get(res=>{
+//         if(res.data.length>0){
+//           wx.showToast({
+//             title: '用户名已存在',
+//             icon:'error',
+//             duration: 1500,
+//           })
+//         }
+//         else {
+//           db.collection("Users")
+//           .limit(1).get(res=>{
+//             this.setData({
+//               IDNext: res.data[0].ID + 1
+//             }) 
+//             db.collection("Users")
+//             .add({
+//               data:{
+//               ID:this.data.IDNext,
+//               Balance:0,
+//               Name:this.data.Account_new,
+//               Password:this.data.Password_new,
+//               Photo_link:'' //默认头像路径
+//               }
+//             })
+//             .get(res=>{
+//               app.globalData.User = res.data
+//             })
+//             wx.reLaunch({
+//               url:"../index/index",
+//             })
+//             wx.showToast({
+//               title: '注册成功',
+//               icon: 'success',
+//               duration: 1500,//持续的时间
+//             })
+//           })
+//         }
+//       })
+//     }
   },
 
   Weixin_login :function() {
@@ -98,48 +158,6 @@ Page({
       // })
     }
   },
-      Account(e){
-          var Account = e.detail.value
-          this.setData({
-            Account
-          })
-        },
-        Password(e){
-          var Password = e.detail.value
-          this.setData({
-            Password
-          })
-        },
-        Account_new(e){
-            var Account_new = e.detail.value
-            this.setData({
-              Account_new
-            })
-          },
-          Password_new(e){
-            var Password_new = e.detail.value
-            this.setData({
-              Password_new
-            })
-          },
-          Password_new_again(e){
-            var Password_new_again = e.detail.value
-            this.setData({
-              Password_new_again
-            })
-          },
-
-  login(){
-    /*
-    查找数据库where name = Account，判断密码一致与否，
-    出错则设置Account或Password_wrong为true
-    *
-    一致跳转页面到index,设置app.globalData.user为获取到的用户结果（数据库获取到的Json格式）
-    *
-    开始时设置Is_loading为true，结束时设为False
-    */
-    
-  },
 
   Want_register(){
     Is_registering = true;
@@ -150,19 +168,59 @@ Page({
     //开始和结束设置is——loading
   },
 
-  Try_register(){
-    /*
-    判断密码长度是够合法，两次密码是否一致，设置相关布尔值
-    判断Account——new是否合法，比较数据库账户是否已存在，设置相关布尔值  
-    有任何错误则结束
-    *
-    都成功则依据数据库中的ID序号生成下一序号,
-    设置app.globalData.user为新建的用户Json格式数据,
-    并在user添加该用户,最后跳转至index  
-    *
-    开始和结束设置is——loading
-    */
-    Is_loading = true;
+/**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
 
-  },
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
 });
