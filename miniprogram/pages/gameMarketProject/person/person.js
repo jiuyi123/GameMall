f9c1c3e11ca5a7a009702d26e9beaecd5f1b86ea
JY <741026400@qq.com>
2022-05-19 14:13:00 +0800

    我的主页面框架搭建完成
    在person文件夹下分为module和personInfo两个，对应上下两个部分

--- miniprogram/pages/gameMarketProject/person/person.js
+++ miniprogram/pages/gameMarketProject/person/person.js
@@ -5,65 +5,99 @@ Page({
    * 页面的初始数据
    */
   data: {
-      user:{
-        Account:"1094567728",
-        Balance:"99",
-        ID:"000001",
-        Name:"JY",
-        Password:"123456",
-        PhotoLink:"https://s1.ax1x.com/2022/05/08/O1XIk4.jpg",
-      
-      }
+    user: {
+      Account: "1094567728",
+      Balance: "99",
+      ID: "000001",
+      Name: "JY",
+      Password: "123456",
+      PhotoLink: "https://s1.ax1x.com/2022/05/08/O1XIk4.jpg",
+
+    }
   },
   //购物车
-  goShopCart(e){
+  goShopCart(e) {
     console.log("goShopCart")
     // console.log(e)
     // var gameInfoStr = encodeURIComponent(JSON.stringify(e.currentTarget.dataset.gameInfo)) 
     //把点击的游戏对象参数传递给游戏详情页面
     wx.navigateTo({
-      url:"/pages/gameMarketProject/person/modules/shopCart/shopCart",
+      url: "/pages/gameMarketProject/person/modules/shopCart/shopCart",
     })
   },
   //收藏
-  goFavorite(e){
+  goFavorite(e) {
     console.log("goFavorite")
     wx.navigateTo({
-      url:"/pages/gameMarketProject/person/modules/favorite/favorite",
+      url: "/pages/gameMarketProject/person/modules/favorite/favorite",
     })
   },
   //订单
-  goOrder(e){
+  goOrder(e) {
     console.log("goOrder")
     wx.navigateTo({
-      url:"/pages/gameMarketProject/person/modules/order/order",
+      url: "/pages/gameMarketProject/person/modules/order/order",
     })
   },
-//仓库
-goWarehouse(e){
-  console.log("goWarehouse")
-  wx.navigateTo({
-    url:"/pages/gameMarketProject/person/modules/warehouse/warehouse",
-  })
-},
-
-//登出
-Logout(){
-  wx.showModal({
-    title: '提示',
-    content: '确定退出登录嘛',
-    success: function (res) {
-      if (res.confirm) {//这里是点击了确定以后
-        wx.reLaunch({
-          url:"../login/login",
-        })
-      } else {//这里是点击了取消以后
-        console.log('用户点击取消')
+  //仓库
+  goWarehouse(e) {
+    console.log("goWarehouse")
+    wx.navigateTo({
+      url: "/pages/gameMarketProject/person/modules/warehouse/warehouse",
+    })
+  },
+  //账户
+  goAccount() {
+    console.log("goAccount")
+    wx.navigateTo({
+      url: "/pages/gameMarketProject/person/personInfo/account/account",
+    })
+  },
+  //历史记录
+  goHistory() {
+    console.log("goHistory")
+    wx.navigateTo({
+      url: "/pages/gameMarketProject/person/personInfo/history/history",
+    })
+  },
+  //我的好友
+  goFriend() {
+    console.log("goFriend")
+    wx.navigateTo({
+      url: "/pages/gameMarketProject/person/personInfo/friend/friend",
+    })
+  },
+  //意见反馈
+  goFeedback() {
+    console.log("goFeedback")
+    wx.navigateTo({
+      url: "/pages/gameMarketProject/person/personInfo/feedback/feedback",
+    })
+  },
+  //设置
+  goSettings() {
+    console.log("goSettings")
+    wx.navigateTo({
+      url: "/pages/gameMarketProject/person/personInfo/settings/settings",
+    })
+  },
+  //登出
+  Logout() {
+    wx.showModal({
+      title: '提示',
+      content: '确定退出登录嘛',
+      success: function (res) {
+        if (res.confirm) { //这里是点击了确定以后
+          wx.reLaunch({
+            url: "../login/login",
+          })
+        } else { //这里是点击了取消以后
+          console.log('用户点击取消')
+        }
       }
-    }
-  })
+    })
 
-},
+  },
   /**
    * 生命周期函数--监听页面加载
    */
