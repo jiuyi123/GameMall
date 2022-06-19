91950b76dd5062034e2b83f997202ed09cf29771
ZiLong W <1074787609@qq.com>
2022-06-20 07:48:38 +0800

    排行榜函数
    增加了排行榜函数，对于各个标签都设置了相应的排行榜，记录了该标签下得分前10的游戏

--- miniprogram/pages/gameMarketProject/index/gameSort/sort/sort.js
+++ miniprogram/pages/gameMarketProject/index/gameSort/sort/sort.js
@@ -156,11 +156,34 @@ Page({
     pslist: {},
     scrolltop: 0,
     flavor: '',
-    heightArray: '900'
+    heightArray: '900',
+
+    //游戏列表
+    Game_list: '',
+    celueyouxi:[],
+    disanrenchensheji:[],
+    diyirenchensheji:[],
+    dongzuojuese:[],
+    dongzuoyouxi:[],
+    gedouyouxi:[],
+    jishizhanlue:[],
+    juesebanyan:[],
+    maoxianyouxi:[],
+    monijingying:[],
+    moniqilei:[],
+    qitayouxi:[],
+    saicheyouxi :[],
+    shejiyouxi:[],
+    tiyuyundong:[],
+    wangluoyouxi:[],
+    xiuxianyizhi:[],
+    yangchengyouxi:[],
+    yinyueyouxi:[],
+    zhuomianqipai:[]
   },
 
-    //游戏详情页面
-    goDetail(e){
+  //游戏详情页面
+  goDetail(e){
       console.log("GoDetail")
       // console.log(e)
       // console.log("-----------------")
@@ -183,12 +206,143 @@ Page({
       wx.navigateTo({
         url:"/pages/gameMarketProject/index/gameDetail/detail/detail?gameInfoStr="+gameInfoStr,
       })
-    },
+   },
 
+  SortGame: function(){
+    this.setData({
+      Game_list: app.globalData.Game
+    })
+
+    var game;//当前读取的游戏对象
+    for (var i = 0; i < this.data.Game_list.length; i++) {
+      game = this.data.Game_list[i];
+      if(game.Score == "null") continue;//没有打分直接跳过
+      switch (game.Tag) {
+        case "策略游戏":
+          this.InsertGameToArray(game,this.data.celueyouxi);
+          break;
+        case "第三人称射击":
+          this.InsertGameToArray(game,this.data.disanrenchensheji);
+          break;
+        case "第一人称射击":
+          this.InsertGameToArray(game,this.data.diyirenchensheji);
+          break;  
+        case "动作角色":
+          this.InsertGameToArray(game,this.data.dongzuojuese);
+          break;    
+        case "动作游戏":
+          this.InsertGameToArray(game,this.data.dongzuoyouxi);
+          break;
+        case "格斗游戏":
+          this.InsertGameToArray(game,this.data.gedouyouxi);
+          break;
+        case "即时战略":
+          this.InsertGameToArray(game,this.data.jishizhanlue);
+          break;
+        case "角色扮演":
+          this.InsertGameToArray(game,this.data.juesebanyan);
+          break;
+        case "冒险游戏":
+          this.InsertGameToArray(game,this.data.maoxianyouxi);
+          break;
+        case "模拟经营":
+          this.InsertGameToArray(game,this.data.monijingying);
+          break;
+        case "模拟器类":
+          this.InsertGameToArray(game,this.data.moniqilei);
+          break;
+        case "其他游戏":
+          this.InsertGameToArray(game,this.data.qitayouxi);
+          break;
+        case "赛车游戏":
+          this.InsertGameToArray(game,this.data.saicheyouxi);
+          break;
+        case "射击游戏":
+          this.InsertGameToArray(game,this.data.shejiyouxi);
+          break;
+        case "体育运动":
+          this.InsertGameToArray(game,this.data.tiyuyundong);
+          break;
+        case "网络游戏":
+          this.InsertGameToArray(game,this.data.wangluoyouxi);
+          break;
+        case "休闲益智":
+          this.InsertGameToArray(game,this.data.xiuxianyizhi);
+          break;
+        case "养成游戏":
+          this.InsertGameToArray(game,this.data.yangchengyouxi);
+          break;
+        case "音乐游戏":
+          this.InsertGameToArray(game,this.data.yinyueyouxi);
+          break;
+        case "桌面棋牌":
+          this.InsertGameToArray(game,this.data.zhuomianqipai);
+          break;
+        default:
+          break;
+      }
+    }
+
+    //test
+    console.log("动作游戏：");
+    for (var i = 0; i < this.data.dongzuoyouxi.length; i++){
+      console.log(i+"."+this.data.dongzuoyouxi[i].Name+":"+this.data.dongzuoyouxi[i].Score+" ")
+    }
+    console.log("策略游戏：");
+    for (var i = 0; i < this.data.celueyouxi.length; i++){
+      console.log(i+"."+this.data.celueyouxi[i].Name+":"+this.data.dongzuoyouxi[i].Score+" ")
+    }
+  },
+  
+  //把当前读取到的游戏对象 从大到小 的方式插入到对应的游戏列表中
+  InsertGameToArray: function(game,gamelist){
+    var ListLength = 10;//列表长度限制
+    //表满的情况
+    if(gamelist.length == ListLength || gamelist.length == ListLength + 1){
+      if(game.Score > gamelist[ListLength - 1].Score){//游戏得分大于列表中最低得分
+        for(var i = 0; i < ListLength; i++){//寻找插入位置
+          if(game.Score < gamelist[i].Score) continue;//没找到
+          else{//找到,从当前要插入的位置向后顺移 
+            var j = ListLength - 1;
+            while(j >= i){//依次后移
+              gamelist[j+1] = gamelist[j];
+              j--;
+            } 
+            gamelist[i]=game;//找到的当前位置存放该游戏对象
+            break;
+          }
+        }
+      }
+      return ;
+    }
+    //表空的情况
+    if(gamelist.length == 0){
+      gamelist[0] = game;
+      return ;
+    }
+    //表非空且未满的情况
+    if(game.Score > gamelist[gamelist.length - 1].Score){//游戏得分大于列表中最低得分
+      for(var i = 0; i < gamelist.length; i++){//寻找插入位置
+        if(game.Score < gamelist[i].Score) continue;//没找到
+        else{//找到,从当前要插入的位置向后顺移 
+          var j = gamelist.length - 1;
+          while(j >= i){//依次后移
+            gamelist[j+1] = gamelist[j];
+            j--;
+          } 
+          gamelist[i]=game;//找到的当前位置存放该游戏对象
+          break;
+        }
+      }
+    }else{
+      gamelist[gamelist.length] = game;
+    }
+  },
 
   onLoad: function (options) {
     // var id = this.data.id
     this.getCurrentCategory(0);
+    this.SortGame();
     // this.getCatalog();
     // id.forEach((item, index) => {
     //   console.log(item)
