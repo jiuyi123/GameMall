f04d9db20f6b0811628b32a61d7b3bcba0e9908f
花园路 <1262525903@qq.com>
2022-06-25 18:44:49 +0800

    历史记录页面

--- miniprogram/pages/gameMarketProject/person/personInfo/history/history.js
+++ miniprogram/pages/gameMarketProject/person/personInfo/history/history.js
@@ -1,18 +1,52 @@
 // pages/gameMarketProject/person/personInfo/history/history.js
+const app = getApp()
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    Show_list:[]
   },
 
+  check(gameid,show_list,time){
+    for(var i = 0;i < show_list.length;i++){
+      if(show_list[i].ID == gameid) {
+        show_list[i].Time = time
+        return true
+      }
+      else continue
+    }
+    return false
+  },
+
+  async LoadInfo(){
+    let count = await db.collection("Click").where({      User_ID:app.globalData.User[0].ID}).count()
+    count = count.total
+    let all = []
+    for(let i = 0; i < count; i += 20){
+      let list = await db.collection("Click").where({      User_ID:app.globalData.User[0].ID}).skip(i).get()
+      all = all.concat(list.data)
+    }
+    let show_list = []
+    count = 0
+    for(var i = 0;i < all.length;i++){
+      if(!this.check(all[i].Game_ID,show_list,all[i].Time)&&all[i].length!=4){
+        show_list = show_list.concat(app.globalData.Game[all[i].Game_ID-1])
+        show_list[count++].Time = all[i].Time
+      }
+    }
+    this.setData({
+      Show_list:show_list
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
+  async onLoad(options) {
+    await this.LoadInfo()
+    console.log(this.data.Show_list)
   },
 
   /**
