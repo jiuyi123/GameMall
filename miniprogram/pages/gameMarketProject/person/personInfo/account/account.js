80a52d78f65a39ccbc5b4235adc78c29d13b02b6
花园路 <1262525903@qq.com>
2022-06-26 16:35:06 +0800

    我的信息页面信息修改

--- miniprogram/pages/gameMarketProject/person/personInfo/account/account.js
+++ miniprogram/pages/gameMarketProject/person/personInfo/account/account.js
@@ -1,5 +1,6 @@
 // pages/gameMarketProject/person/personInfo/account/account.js
 const app = getApp();
+var db = wx.cloud.database();
 Page({
   /**
    * 页面的初始数据
@@ -8,31 +9,67 @@ Page({
     user: "",
     newEmail: "",
     newNickname: "",
-    newCountry:""
+    newCountry: ""
   },
-  EmailChage(e) {
-    this.setData({
-      newEmail: e.detail.value
-    })
-  },
-  NicknameChage(e) {
-    this.setData({
-      newNickname: e.detail.value
-    })
-  },
+
   CountryChage(e) {
     this.setData({
       newCountry: e.detail.value
     })
   },
-  setNickname() {
-    console.log(this.data.newNickname)
+
+  EmailChage(e) {
+    this.setData({
+      newEmail: e.detail.value
+    })
   },
+
+  NicknameChage(e) {
+    this.setData({
+      newNickname: e.detail.value
+    })
+  },
+
+  async setNickname() {
+    console.log(this.data.newNickname)
+    db.collection("Users").doc(this.data.user._id).update({
+      data: {
+        Nickname: this.data.newNickname
+      }
+    })
+    wx.showToast({
+      title: '昵称修改成功',
+      icon: 'success',
+      duration: 900, //持续的时间
+    })
+  },
+
   setEmail() {
     console.log(this.data.newEmail)
+    db.collection("Users").doc(this.data.user._id).update({
+      data: {
+        Email: this.data.newEmail
+      }
+    })
+    wx.showToast({
+      title: '邮箱修改成功',
+      icon: 'success',
+      duration: 900, //持续的时间
+    })
   },
+
   setCountry() {
     console.log(this.data.newCountry)
+    db.collection("Users").doc(this.data.user._id).update({
+      data: {
+        Country: this.data.newCountry
+      }
+    })
+    wx.showToast({
+      title: '地区信息修改成功',
+      icon: 'success',
+      duration: 900, //持续的时间
+    })
   },
   /**
    * 生命周期函数--监听页面加载
@@ -42,7 +79,7 @@ Page({
       //加载用户信息
       user: app.globalData.User[0]
     });
-    console.log(this.data.user)
+    //console.log(this.data.user)
   },
 
   /**
