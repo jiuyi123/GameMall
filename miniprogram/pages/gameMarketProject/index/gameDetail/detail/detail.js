b62fdb34f05b8ec6f046f05dba022fef35de987f
ZiLong W <1074787609@qq.com>
2022-06-27 23:42:32 +0800

    完成了评论数据库的写入部分后端

--- miniprogram/pages/gameMarketProject/index/gameDetail/detail/detail.js
+++ miniprogram/pages/gameMarketProject/index/gameDetail/detail/detail.js
@@ -1,31 +1,32 @@
 // pages/gameMarketProject/index/gameDetail/detail/detail.js
-var app = getApp()
-var db = wx.cloud.database()
+var app = getApp();
+var db = wx.cloud.database();
 var gameInfoObj
 var game_id
 
 Page({
+
   /**
    * 页面的初始数据
    */
   data: {
-    gameInfoObj: {},
+    gameInfoObj: {
+
+    },
     /*******购买弹窗*******/
-    radio: '1', //单选框
-    checked: false, //复选框
+    radio: '1',//单选框
+    checked: false,//复选框
     show: false,
     Buying: false,
-    PayWay: 0, //购买方式 0:余额购买 1-n：其他方式
-    actions: [
-      {
-        name: '获取用户信息',
-        color: '#07c160',
-        openType: 'getUserInfo'
-      }
-    ],
+    PayWay: 0,//购买方式 0:余额购买 1-n：其他方式
+    actions: [{
+      name: '获取用户信息',
+      color: '#07c160',
+      openType: 'getUserInfo'
+    }, ],
     /******信息页面数据*******/
     activeNames: ['1'], //折叠面板
-    winHeight: '', //窗口高度
+    winHeight: "", //窗口高度
     currentTab: 0, //预设当前项的值
     scrollLeft: 0, //tab标题的滚动条位置
     /*************/
@@ -37,46 +38,47 @@ Page({
   /****************购买弹窗****************/
   onChangeCheckbox(event) {
     this.setData({
-      checked: event.detail
-    })
+      checked: event.detail,
+    });
   },
   onChangeRadio(event) {
     this.setData({
-      PayWay: event.detail
-    })
+      radio: event.detail,
+    });
   },
-  buyPop() {
+  buyPop()
+  {
     this.setData({
-      Buying: true
-    })
+      show: true
+    });
   },
   onClose() {
     this.setData({
-      Buying: false
-    })
+      show: false
+    });
   },
   onGetUserInfo(e) {
-    console.log(e.detail)
+    console.log(e.detail);
   },
   /**************游戏简介等折叠面板************/
   onChange(event) {
     this.setData({
-      activeNames: event.detail
-    })
+      activeNames: event.detail,
+    });
   },
   /**************中部标签设计***************** */
   // 滚动切换标签样式
   switchTab: function (e) {
     this.setData({
       currentTab: e.detail.current
-    })
-    this.checkCor()
+    });
+    this.checkCor();
   },
   // 点击标题切换当前页时改变样式
   swichNav: function (e) {
-    var cur = e.target.dataset.current
+    var cur = e.target.dataset.current;
     if (this.data.currentTaB == cur) {
-      return false
+      return false;
     } else {
       this.setData({
         currentTab: cur
@@ -100,61 +102,60 @@ Page({
   Load(gameInfoObj) {
     gameInfoObj.numComment = 999
     gameInfoObj.score = 4.8
-    gameInfoObj.comment = [
-      {
-        userName: 1,
-        cmtData: '2021.04.01 14:08',
-        cmtScore: [0, 1, 2, 3],
-        cmtContent: '这个游戏太好玩了'
+    gameInfoObj.comment = [{
+        "userName": 1,
+        "cmtData": "2021.04.01 14:08",
+        "cmtScore": [0, 1, 2, 3],
+        "cmtContent": "这个游戏太好玩了"
       },
       {
-        userName: 2,
-        cmtData: '2021.06.01 14:08',
-        cmtScore: [0, 2],
-        cmtContent: '还可以吧'
+        "userName": 2,
+        "cmtData": "2021.06.01 14:08",
+        "cmtScore": [0, 2],
+        "cmtContent": "还可以吧"
       },
       {
-        userName: 3,
-        cmtData: '2021.04.12 14:08',
-        cmtScore: [0, 1, 2],
-        cmtContent: '画风我爱了'
+        "userName": 3,
+        "cmtData": "2021.04.12 14:08",
+        "cmtScore": [0, 1, 2],
+        "cmtContent": "画风我爱了"
       },
       {
-        userName: 5,
-        cmtData: '2021.04.13 14:08',
-        cmtScore: [0, 1, 2, 4, 5],
-        cmtContent: '强烈推荐'
+        "userName": 5,
+        "cmtData": "2021.04.13 14:08",
+        "cmtScore": [0, 1, 2, 4, 5],
+        "cmtContent": "强烈推荐"
       },
       {
-        userName: 6,
-        cmtData: '2021.04.13 14:08',
-        cmtScore: [0, 1, 2, 4, 5],
-        cmtContent: '强烈推荐'
+        "userName": 6,
+        "cmtData": "2021.04.13 14:08",
+        "cmtScore": [0, 1, 2, 4, 5],
+        "cmtContent": "强烈推荐"
       },
       {
-        userName: 7,
-        cmtData: '2021.04.13 14:08',
-        cmtScore: [0, 1, 2, 4, 5],
-        cmtContent: '强烈推荐'
+        "userName": 7,
+        "cmtData": "2021.04.13 14:08",
+        "cmtScore": [0, 1, 2, 4, 5],
+        "cmtContent": "强烈推荐"
       }
     ]
     this.setData({
-      gameInfoObj
+      gameInfoObj,
     })
     console.log(this.data.gameInfoObj)
     // 高度自适应
-    var that = this
+    var that = this;
     wx.getSystemInfo({
       success: function (res) {
         var clientHeight = res.windowHeight,
           clientWidth = res.windowWidth,
-          rpxR = 750 / clientWidth
-        var calc = clientHeight * rpxR - 380
+          rpxR = 750 / clientWidth;
+        var calc = clientHeight * rpxR - 380;
         that.setData({
           winHeight: calc
-        })
+        });
       }
-    })
+    });
   },
 
   async GetAlldb(DBName) {
@@ -169,21 +170,20 @@ Page({
   },
 
   GetTime() {
-    var blank = ''
-    var myDate = new Date()
-    var year = myDate.getFullYear()
-    var month = myDate.getMonth() + 1 < 10 ? '0' + (myDate.getMonth() + 1) : myDate.getMonth() + 1
-    var date = myDate.getDate() < 10 ? '0' + myDate.getDate() : myDate.getDate()
-    var hour = myDate.getHours() < 10 ? '0' + myDate.getHours() : myDate.getHours()
-    var min = myDate.getMinutes() < 10 ? '0' + myDate.getMinutes() : myDate.getMinutes()
-    var sec = myDate.getSeconds() < 10 ? '0' + myDate.getSeconds() : myDate.getSeconds()
-    var myTime = blank.concat(year, '-', month, '-', date, ' ', hour, ':', min, ':', sec)
+    var blank = ""
+    var myDate = new Date();
+    var year = myDate.getFullYear();
+    var month = (myDate.getMonth() + 1 < 10 ? '0' + (myDate.getMonth() + 1) : myDate.getMonth() + 1);
+    var date = myDate.getDate() < 10 ? '0' + myDate.getDate() : myDate.getDate();
+    var hour = myDate.getHours() < 10 ? '0' + myDate.getHours() : myDate.getHours();
+    var min = myDate.getMinutes() < 10 ? '0' + myDate.getMinutes() : myDate.getMinutes();
+    var sec = myDate.getSeconds() < 10 ? '0' + myDate.getSeconds() : myDate.getSeconds();
+    var myTime = blank.concat(year, "-", month, "-", date, " ", hour, ":", min, ":", sec);
     return myTime
   },
 
   async Is_ShouCang(game_id) {
-    const res = await db
-      .collection('Collect')
+    const res = await db.collection("Collect")
       .where({
         Game_ID: game_id,
         User_ID: app.globalData.User[0].ID
@@ -197,18 +197,17 @@ Page({
   },
 
   async Is_GouWuCheGouMai(game_id) {
-    const res = await db
-      .collection('Orders')
+    const res = await db.collection("Orders")
       .where({
         Game_ID: game_id,
         User_ID: app.globalData.User[0].ID
       })
       .get()
-    if (res.data.length > 0 && res.data[0].State == '未支付') {
+    if (res.data.length > 0 && res.data[0].State == "未支付") {
       this.setData({
         is_GouWuChe: true
       })
-    } else if (res.data.length > 0 && res.data[0].State == '已支付') {
+    } else if (res.data.length > 0 && res.data[0].State == "已支付") {
       this.setData({
         is_GouMai: true
       })
@@ -216,21 +215,16 @@ Page({
   },
 
   async UpdateData() {
-    var res = await db
-      .collection('Games')
-      .where({
-        ID: game_id
-      })
-      .get()
+    var res = await db.collection("Games").where({
+      ID: game_id
+    }).get()
     console.log(res.data)
-    db.collection('Games')
-      .doc(res.data[0]._id)
-      .update({
-        data: {
-          Hits: res.data[0].Hits + 1
-        }
-      })
-    db.collection('Click').add({
+    db.collection("Games").doc(res.data[0]._id).update({
+      data: {
+        Hits: res.data[0].Hits + 1
+      }
+    })
+    db.collection("Click").add({
       data: {
         Game_ID: game_id,
         Time: this.GetTime(),
@@ -241,14 +235,11 @@ Page({
 
   async ShouCang() {
     if (this.data.is_ShouCang) {
-      var res = await db
-        .collection('Collect')
-        .where({
-          Game_ID: game_id,
-          User_ID: app.globalData.User[0].ID
-        })
-        .get()
-      db.collection('Collect').doc(res.data[0]._id).remove()
+      var res = await db.collection("Collect").where({
+        Game_ID: game_id,
+        User_ID: app.globalData.User[0].ID
+      }).get()
+      db.collection("Collect").doc(res.data[0]._id).remove()
       this.setData({
         is_ShouCang: false
       })
@@ -258,7 +249,7 @@ Page({
         duration: 1000
       })
     } else {
-      db.collection('Collect').add({
+      db.collection("Collect").add({
         data: {
           Game_ID: game_id,
           Time: this.GetTime(),
@@ -276,44 +267,50 @@ Page({
     }
   },
 
-  async Gouwuche() {
-    //函数内不允许添加await待测试
-    const res = await db
-      .collection('Orders')
-      .where({
-        Game_ID: game_id,
-        User_ID: app.globalData.User[0].ID
-      })
-      .get()
+  async Goumai() {
+    const res = await db.collection("Orders").where({
+      Game_ID: game_id,
+      User_ID: app.globalData.User[0].ID
+    }).get()
     const that = this
-    const data = await this.GetAlldb('Orders')
-    if (this.data.is_GouWuChe) {
+    const data = await this.GetAlldb("Orders")
+    if (this.data.is_GouMai == false) {
       wx.showModal({
-        title: '警告',
-        content: '是否要将产品移除购物车',
+        title: '确认',
+        content: '是否购买此产品',
         success(r) {
           //如果用户点击了确定按钮
           if (r.confirm) {
-            db.collection('Orders').doc(res.data[0]._id).remove()
+            if (res.data.length > 0) { //记录存在说明在购物车内
+              db.collection("Orders").doc(res.data[0]._id).update({
+                data: {
+                  State: "已支付",
+                  Time: that.GetTime()
+                }
+              })
+            } else { //记录不在购物车内
+              db.collection("Orders")
+                .add({
+                  data: {
+                    Final_price: gameInfoObj.Price,
+                    Game_ID: game_id,
+                    ID: data[data.length - 1].ID + 1,
+                    State: "已支付",
+                    Time: that.GetTime(),
+                    User_ID: app.globalData.User[0].ID
+                  },
+                })
+            }
             that.setData({
-              is_GouWuChe: false
+              is_GouMai: true
             })
+          //  this.onClose()
+          //  wx.showToast({
+          //    title: '购买成功',
+          //    duration:1000
+          //  })
           }
         }
-      })
-    } else {
-      db.collection('Orders').add({
-        data: {
-          Final_price: gameInfoObj.Price,
-          Game_ID: game_id,
-          ID: data[data.length - 1].ID + 1,
-          State: '未支付',
-          Time: this.GetTime(),
-          User_ID: app.globalData.User[0].ID
-        }
-      })
-      this.setData({
-        is_GouWuChe: true
       })
     }
   },
@@ -322,71 +319,72 @@ Page({
     this.setData({
       WantBuy: true
     })
-    if (this.data.PayWay == 0) {
-      //余额支付
-      var game = await db
-        .collection('Games')
-        .where({
-          ID: app.globalData.Game[0].ID
-        })
-        .get()
-      if (app.globalData.User[0].Balance > this.data.gameInfoObj.Price) {
+    if(this.data.PayWay == 0){//余额支付
+      var game = await db.collection("Games").where({
+        ID: app.globalData.Game[0].ID
+      }).get()
+      if(app.globalData.User[0].Balance > this.data.gameInfoObj.Price){
         //修改数据库User
-        var res = await db
-          .collection('Users')
-          .where({
-            ID: app.globalData.User[0].ID
-          })
-          .get()
-        await db
-          .collection('Users')
-          .doc(res.data[0]._id)
-          .update({
-            data: {
-              Balance: app.globalData.User[0].Balance - this.data.gameInfoObj.Price
-            }
-          })
-        res = await db
-          .collection('Users')
-          .where({
-            User_ID: app.globalData.User[0].ID
-          })
-          .get()
+        var res = await db.collection("Users").where({
+          ID: app.globalData.User[0].ID
+        }).get()
+        await db.collection("Users").doc(res.data[0]._id).update({
+          data:{
+            Balance:app.globalData.User[0].Balance-this.data.gameInfoObj.Price
+          }
+        })
+        res = await db.collection("Users").where({
+          User_ID: app.globalData.User[0].ID
+        }).get()
         //增加一条订单
-        const data = await this.GetAlldb('Orders')
-        db.collection('Orders').add({
+        const data = await this.GetAlldb("Orders")
+        db.collection("Orders")
+        .add({
           data: {
             Final_price: gameInfoObj.Price,
             Game_ID: game_id,
             ID: data[data.length - 1].ID + 1,
-            State: '已支付',
+            State: "已支付",
             Time: this.GetTime(),
             User_ID: app.globalData.User[0].ID
-          }
+          },
         })
         app.globalData.User = res.data
         wx.showToast({
-          title: '购买成功'
+          title: '购买成功',
         })
-      } else {
-        //余额不足
+      }else{//余额不足
         wx.showToast({
           title: '余额不足',
-          icon: 'error'
+          icon:'error',
         })
       }
     }
-    this.setData({
-      is_GouMai: true
-    })
     this.BuyClose()
   },
 
-  BuyClose: function () {
+  BuyClose: function(){
     this.setData({
-      Buying: false
+      WantBuy: false
     })
   },
+
+  WriteEvaluation: async function(content, score, game_id){//向数据库中写入评价
+    const cmt = await this.GetAlldb("Comments")
+    console.log(cmt)
+    db.collection("Comments").add({
+      data: {
+        Content: content,
+        Game_ID: game_id,
+        ID: cmt.data[cmt.data.length - 1].ID + 1,
+        Score: score,
+        Time: this.GetTime(),
+        User_ID: app.globalData.User[0].ID
+      },
+    })
+  },
+
+
 
   /********************************************* */
   /**
@@ -410,54 +408,56 @@ Page({
     this.Load(gameInfoObj)
     await this.Is_ShouCang(game_id)
     await this.Is_GouWuCheGouMai(game_id)
+    //await this.WantBuy()
+    await this.WriteEvaluation("爱神的箭哦啊",8.8,4)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    console.log('-----------onReady-----------')
+    console.log("-----------onReady-----------")
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    console.log('-----------onShow-----------')
+    console.log("-----------onShow-----------")
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    console.log('-----------onHide-----------')
+    console.log("-----------onHide-----------")
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    console.log('-----------onUnload-----------')
+    console.log("-----------onUnload-----------")
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    console.log('-----------onPullDownRefresh-----------')
+    console.log("-----------onPullDownRefresh-----------")
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    console.log('-----------onReachBottom-----------')
+    console.log("-----------onReachBottom-----------")
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    console.log('-----------onShareAppMessage-----------')
+    console.log("-----------onShareAppMessage-----------")
   }
 })
