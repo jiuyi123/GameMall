242306c8b0852288be135131016dbc9cb187aa3b
花园路 <1262525903@qq.com>
2022-06-27 21:58:40 +0800

    钱包充值+聊天信息删除
    保证聊天信息始终控制在50条以内

--- miniprogram/pages/gameMarketProject/person/personInfo/wallet/wallet.js
+++ miniprogram/pages/gameMarketProject/person/personInfo/wallet/wallet.js
@@ -1,66 +1,68 @@
 // pages/gameMarketProject/person/personInfo/feedback/feedback.js
+const app = getApp()
+const db = wx.cloud.database()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    Amount: 5
+  },
 
+  async Charge() {
+    let res = await db
+      .collection('Users')
+      .where({
+        ID: app.globalData.User[0].ID
+      })
+      .get()
+    db.collection('Users')
+      .doc(res.data[0]._id)
+      .update({
+        data: {
+          Balance: app.globalData.User[0].Balance + this.data.Amount
+        }
+      })
+    app.globalData.User[0].Balance = app.globalData.User[0].Balance + this.data.Amount
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
-  },
+  onLoad(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
+  onShareAppMessage() {}
 })
