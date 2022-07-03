40445679425dc3bd85c9d06a34c002ff1980282a
宋世城 <741026400@qq.com>
2022-07-03 14:11:10 +0800

    设置界面颜色修改

--- miniprogram/pages/gameMarketProject/information/modules/commentInfo/commentInfo.js
+++ miniprogram/pages/gameMarketProject/information/modules/commentInfo/commentInfo.js
@@ -8,72 +8,6 @@ Page({
   data: {
     winHeight: '', //聊天列表窗口高度
     ShowList: '',
-    //这里本应该是其他用户的消息
-    gameInfoObj: [
-      {
-        userName: 'jy',
-        userPhoto: 'https://s1.ax1x.com/2022/05/08/O1XIk4.jpg',
-        infoContent: '今天有时间呀',
-        infoTime: '13:15',
-        numCommentInfo: 12
-      },
-      {
-        userName: 'ctz',
-        userPhoto: 'https://s1.ax1x.com/2022/05/18/OTqQZ8.jpg ',
-        infoContent: '今天有时间呀',
-        infoTime: '13:15',
-        numCommentInfo: 5
-      },
-      {
-        userName: 'wzl',
-        userPhoto: 'https://s1.ax1x.com/2022/05/18/OTqKqf.jpg',
-        infoContent: '今天有时间呀',
-        infoTime: '13:15',
-        numCommentInfo: 1
-      },
-      {
-        userName: 'ycr',
-        userPhoto: 'https://s1.ax1x.com/2022/05/18/OTqnMt.jpg',
-        infoContent: '今天有时间呀',
-        infoTime: '13:15',
-        numCommentInfo: 999
-      },
-      {
-        userName: 'hyl',
-        userPhoto: 'https://s1.ax1x.com/2022/05/18/OTqusP.jpg',
-        infoContent: '今天有时间呀',
-        infoTime: '13:15',
-        numCommentInfo: 0
-      },
-      {
-        userName: '小久',
-        userPhoto: 'https://s1.ax1x.com/2022/05/08/O1XIk4.jpg',
-        infoContent: '今天有时间呀',
-        infoTime: '13:15',
-        numCommentInfo: 0
-      },
-      {
-        userName: '小楠',
-        userPhoto: 'https://s1.ax1x.com/2022/05/08/O1XIk4.jpg',
-        infoContent: '今天有时间呀',
-        infoTime: '13:15',
-        numCommentInfo: 0
-      },
-      {
-        userName: '康康',
-        userPhoto: 'https://s1.ax1x.com/2022/05/08/O1XIk4.jpg',
-        infoContent: '今天有时间呀',
-        infoTime: '13:15',
-        numCommentInfo: 0
-      },
-      {
-        userName: '小云',
-        userPhoto: 'https://s1.ax1x.com/2022/05/08/O1XIk4.jpg',
-        infoContent: '今天有时间呀',
-        infoTime: '13:15',
-        numCommentInfo: 0
-      }
-    ]
   },
 
   // 跳转聊天界面
@@ -208,6 +142,7 @@ Page({
     this.setData({
       ShowList: list
     })
+    console.log("list")
     console.log(list)
   },
   /**
